45c0551b72ce1c8d86f6db9a3a0c7a722184db8a
Vincent Bonamy <vbonamy@gmail.com>
2012-07-05 16:59:35 +0200

    remoteuservalve 1.0.0 OK

---
+++ src/main/java/fr/runn/catalina/valves/RemoteUserValve.java
@@ -0,0 +1,30 @@
+package fr.runn.catalina.valves;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.connector.CoyotePrincipal;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RemoteUserValve extends ValveBase {
+	
+	private static final Log log = LogFactory.getLog(RemoteUserValve.class);
+	
+	public void invoke(Request request, Response response) throws IOException,
+			ServletException {
+		String remoteUser = request.getHeader("REMOTE_USER");
+		Principal userPrincipal = request.getUserPrincipal();
+		if (userPrincipal == null && remoteUser != null && !"".equals(remoteUser)) {
+			request.setUserPrincipal(new CoyotePrincipal(remoteUser));
+			log.debug(remoteUser + " authenticated with REMOTE_USER Request Header");
+		}
+		getNext().invoke(request, response);
+	}
+	
+}
